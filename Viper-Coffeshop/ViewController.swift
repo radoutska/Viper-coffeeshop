e8fd5630c700e0d1b4037ef86c868b09d7bd579b
Anna Radoutska <aradoutska@griddynamics.com>
2022-07-18 20:52:26 -0400

    Initial Commit

---
+++ Viper-Coffeshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper-Coffeshop
+//
+//  Created by Anna Radoutska on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
